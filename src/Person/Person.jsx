af6fafdc0410135f5deb5668ba6681a5e6ba9d31
Angger Priyardhan Putro (Laptop HP) <anggerpputro@gmail.com>
2018-06-23 10:23:56 +0700

    lecture 53: Outputting Lists

--- src/Person/Person.jsx
+++ src/Person/Person.jsx
@@ -5,7 +5,7 @@ export default class Person extends React.Component {
 	render() {
 		return (
 			<div className="person-container">
-				<h1>Hello, my name is {this.props.name}</h1>
+				<h1>Hello, my name is: <span>{this.props.name}</span></h1>
 				<p>I am {this.props.age} years old</p>
 			</div>
 		);
